21e2458538a2cab6a96b0156d041596a8f6be789
Sebastian Widua <seppel3210@gmail.com>
2021-05-30 02:40:33 +0200

    Initial implementation

---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+extern crate proc_macro;
+
+use syn::{parse_macro_input, Expr, ExprBinary, ExprUnary, BinOp, UnOp};
+use quote::{quote, ToTokens};
+use proc_macro2::{TokenTree, Ident, Span, TokenStream};
+
+/// Example of [function-like procedural macro][1].
+///
+/// [1]: https://doc.rust-lang.org/reference/procedural-macros.html#function-like-procedural-macros
+#[proc_macro]
+pub fn checked_expr(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as Expr);
+
+    expr(&input).into()
+}
+
+fn expr(expr: &Expr) -> TokenStream {
+    match expr {
+        Expr::Unary(expr) => { unary_expr(expr) }
+        Expr::Binary(expr) => { binary_expr(expr) }
+        Expr::Paren(expr) => crate::expr(&expr.expr),
+        _ => {
+            let tokens = quote! {
+                Some(#expr)
+            };
+            tokens.into()
+        }
+    }
+}
+
+fn binary_expr(expr: &ExprBinary) -> TokenStream {
+    let function_name = match expr.op {
+        BinOp::Add(_) => "checked_add",
+        BinOp::Sub(_) => "checked_sub",
+        BinOp::Mul(_) => "checked_mul",
+        BinOp::Div(_) => "checked_div",
+        BinOp::Rem(_) => "checked_rem",
+        BinOp::Shl(_) => "checked_shl",
+        BinOp::Shr(_) => "checked_shr",
+        _ => return expr.to_token_stream()
+    };
+    let fn_ident = TokenTree::Ident(Ident::new(function_name, Span::call_site()));
+
+    let left = crate::expr(&expr.left);
+    let right = crate::expr(&expr.right);
+    quote! {
+        #left.and_then(|x| x.#fn_ident(#right?))
+    }
+}
+
+fn unary_expr(expr: &ExprUnary) -> TokenStream {
+    let function_name = match expr.op {
+        UnOp::Neg(_) => "checked_neg",
+        _ => return expr.to_token_stream()
+    };
+    let fn_ident = TokenTree::Ident(Ident::new(function_name, Span::call_site()));
+
+    let inner = crate::expr(&expr.expr);
+    quote! {
+        #inner.and_then(|x| x.#fn_ident())
+    }
+}
